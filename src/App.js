23889b936f4026900c872102f49e351fbdf4b797
hritik Sharma <hritik2805@gmail.com>
2022-06-22 18:01:08 +0530

    UI and expense folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
