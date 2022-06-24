b46ac6e5c6f68b503a14a8f8b019ac88268e0855
hritik Sharma <hritik2805@gmail.com>
2022-06-24 18:48:18 +0530

    .

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
-import React, { useInsertionEffect, useState } from "react";
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
-import "./expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpenseList";
+import "./expenses.css";
+
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
@@ -11,11 +13,7 @@ const Expenses = (props) => {
   };
 
   const filteredExpenses = props.items.filter((expense) => {
-    if (filteredYear === "all") {
-      return expense;
-    } else {
-      return expense.date.getFullYear().toString() === filteredYear;
-    }
+    return expense.date.getFullYear().toString() === filteredYear;
   });
 
   return (
@@ -25,14 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
