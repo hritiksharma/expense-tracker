0041e53d58023b9311503aab636547de95e7b369
hritik Sharma <hritik2805@gmail.com>
2022-06-24 18:53:45 +0530

    .

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,15 @@ import "./expenseItem.css";
 import Card from "../UI/Card";
 const ExpenseItem = ({ title, amount, date }) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-      </div>
-      <div className="expense-item__price">${amount}</div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+        </div>
+        <div className="expense-item__price">${amount}</div>
+      </Card>
+    </li>
   );
 };
 
