24951c88e2be32296ef0f70044313985b410cb51
hritik Sharma <hritik2805@gmail.com>
2022-06-25 11:53:02 +0530

    done remain chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpensesList from "./ExpenseList";
 import "./expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
